0b29fbad6066eac75e29c57856a182db4ac60422
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-12-18 02:24:35 -0200

    Create resolvers for child types and return mock data

--- server.js
+++ server.js
@@ -3,46 +3,52 @@ const { GraphQLServer } = require("graphql-yoga");
 const resolvers = {
   Query: {
     gasStations: (_, { lat, lng }) => {
+      return {};
+    }
+  },
+  GasStationsData: {
+    location: (_, { lat, lng }) => {
       return JSON.parse(`
       {
-          
-          "location": {
-            "lat": -23.5655625,
-            "lng": -46.6472389,
-            "number": "1967",
-            "street": "Avenida Brigadeiro Luís Antônio",
-            "district": "Bela Vista",
-            "city": "São Paulo",
-            "state": "SP",
-            "zipCode": "04002-010"
-          },
-          "gasStations": [
-            {
-              "lat": -23.5597654,
-              "lng": -46.57735239999999,
-              "name": "Shell (Rede Duque)",
-              "address": "Avenida Salim Farah Maluf, 3400 - Vila Bertioga, São Paulo"
-            },
-            {
-              "lat": -23.55331,
-              "lng": -46.6584,
-              "name": "AUTO POSTO BELA CINTRA LTDA",
-              "address": "Rua Fernando de Albuquerque, 216 - Consolação, São Paulo"
-            },
-            {
-              "lat": -23.5300399,
-              "lng": -46.6778527,
-              "name": "Posto Ipiranga",
-              "address": "Rua Cayowaá, 45 - Perdizes, São Paulo"
-            },
-            {
-              "lat": -23.6031717,
-              "lng": -46.6258529,
-              "nome": "Posto Shell",
-              "address": "Avenida Doutor Ricardo Jafet, 3274 - Saúde, São Paulo"
-            }
-          ]
+        "lat": -23.5655625,
+        "lng": -46.6472389,
+        "number": "1967",
+        "street": "Avenida Brigadeiro Luís Antônio",
+        "district": "Bela Vista",
+        "city": "São Paulo",
+        "state": "SP",
+        "zipCode": "04002-010"
+      }
+      `);
+    },
+    gasStations: (_, { lat, lng }) => {
+      return JSON.parse(`
+      [
+        {
+          "lat": -23.5597654,
+          "lng": -46.57735239999999,
+          "name": "Shell (Rede Duque)",
+          "address": "Avenida Salim Farah Maluf, 3400 - Vila Bertioga, São Paulo"
+        },
+        {
+          "lat": -23.55331,
+          "lng": -46.6584,
+          "name": "AUTO POSTO BELA CINTRA LTDA",
+          "address": "Rua Fernando de Albuquerque, 216 - Consolação, São Paulo"
+        },
+        {
+          "lat": -23.5300399,
+          "lng": -46.6778527,
+          "name": "Posto Ipiranga",
+          "address": "Rua Cayowaá, 45 - Perdizes, São Paulo"
+        },
+        {
+          "lat": -23.6031717,
+          "lng": -46.6258529,
+          "nome": "Posto Shell",
+          "address": "Avenida Doutor Ricardo Jafet, 3274 - Saúde, São Paulo"
         }
+      ]
       `);
     }
   }
