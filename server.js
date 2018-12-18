a3059658fc9b6701c94768185fca9be700a5fc21
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-12-18 01:40:03 -0200

    Move schema to .graphql file

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || "World"}`
   }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: "./schema.graphql",
+  resolvers
+});
 server.start(() => console.log("Server is running on localhost:4000"));
